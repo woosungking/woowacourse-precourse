803bc252e6cbe478e469a216a037b5b1bacc9407
woosungking <abuga991008@gmail.com>
2024-11-03 21:14:17 +0900

    feat(DelimitorRepository): 커스텀 구분자를 관리하는 레포지토리 클레스
    기존 구분자 외 커스텀 구분자를 저장하는 변수 delimiterRepo 구현.

---
+++ src/main/java/com/java/memory/delimiter/DelimiterRepository.java
@@ -0,0 +1,23 @@
+package com.java.memory.delimiter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DelimiterRepository {
+    private final List<Delimiter> delimiterRepo = new ArrayList<>();
+
+    public DelimiterRepository() {
+        delimiterRepo.add(new Delimiter(":"));
+        delimiterRepo.add(new Delimiter(","));
+    }
+
+    public void addDelimiter(Delimiter delimiter) {
+        System.out.println(delimiter.getValue());
+        delimiterRepo.add(delimiter);
+    }
+
+    public List<Delimiter> getDelimiterRepo() {
+        return delimiterRepo; // 수정 불가능한 리스트 반환
+    }
+}
