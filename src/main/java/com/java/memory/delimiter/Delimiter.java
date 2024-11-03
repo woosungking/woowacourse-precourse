4cc30d3d156a7e86d9cadf5683a5e6269ff666d3
woosungking <abuga991008@gmail.com>
2024-11-03 21:15:32 +0900

    feat(Delimiter): 커스텀 구분자 클레스 생성
    명확한 의도전달을 위한 구분자 클레스 생성

---
+++ src/main/java/com/java/memory/delimiter/Delimiter.java
@@ -0,0 +1,11 @@
+package com.java.memory.delimiter;
+
+public class Delimiter {
+    private String value;
+    public Delimiter(String value){
+        this.value=value;
+    }
+    public String getValue(){
+        return this.value;
+    }
+}
