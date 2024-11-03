f090e51f801e53df78380c4969ba6c5fe59efec3
woosungking <abuga991008@gmail.com>
2024-11-03 21:22:17 +0900

    feat(InputValueHandlerImpl): 사용자 입력을 받는 서비스 인터페이스

--- src/main/java/com/java/io/InputValueHandler.java
+++ src/main/java/com/java/io/InputValueHandler.java
@@ -1,20 +1,7 @@
 package com.java.io;
 
-import com.java.exception.ErrorCode;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Objects;
 
-public class InputValueHandler {
-    public String readInput() throws IOException {
-
-            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-            String inputValue;
-
-            inputValue = (String)input.readLine();
-            return inputValue;
-    }
+public interface InputValueHandler {
+    public String readInput() throws IOException;
 }
