d820d54ec24fc4f6b37a2a2ea6f103bf605d0438
woosungking <abuga991008@gmail.com>
2024-11-04 21:31:40 +0900

    feat<OutputValueHandler> 결과값의 처리를 도와주는 서비스 인터페이스

---
+++ src/main/java/com/java/service/io/OutputValueHandler.java
@@ -0,0 +1,9 @@
+package com.java.service.io;
+
+import com.java.repository.output.OutputValue;
+
+import java.util.List;
+
+public interface OutputValueHandler {
+    public List<OutputValue> readOutput();
+}
