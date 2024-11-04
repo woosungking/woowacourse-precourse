dfd4c3a0fecb930536c2304e5d0a96fc4024a6b1
woosungking <abuga991008@gmail.com>
2024-11-04 21:58:13 +0900

    refactor<.extractValue> 입력값에서 숫자만 빼오는 메서드

--- src/main/java/com/java/service/calculator/valueextractor/ValueExtractServiceImpl.java
+++ src/main/java/com/java/service/calculator/valueextractor/ValueExtractServiceImpl.java
@@ -1,8 +1,32 @@
 package com.java.service.calculator.valueextractor;
 
+import com.java.repository.delimiter.Delimiter;
+import com.java.repository.delimiter.DelimiterRepository;
+import com.java.repository.operand.Operand;
+import com.java.repository.operand.OperandRepository;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class ValueExtractServiceImpl implements ValueExtractService{
+    private final DelimiterRepository delimiterRepository;
+    private final OperandRepository operandRepository;
+
+    public ValueExtractServiceImpl(DelimiterRepository delimiterRepository, OperandRepository operandRepository) {
+        this.delimiterRepository = delimiterRepository;
+        this.operandRepository = operandRepository;
+    }
+
     @Override
     public void extractValue(String inputValue) {
-
+        List<Delimiter> dex = delimiterRepository.getDelimiterRepo();
+        String regex = String.join("|", dex.stream()
+                .map(Delimiter::getValue)
+                .map(Pattern::quote)
+                .toArray(String[]::new));
+        String[] splitResult = inputValue.split(regex);
+        for(String operand : splitResult){
+            operandRepository.addOperand(new Operand(Integer.parseInt(operand)));
+        }
     }
 }
