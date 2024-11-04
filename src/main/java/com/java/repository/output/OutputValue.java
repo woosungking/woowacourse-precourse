ffc2ef443bc24e0bca1552fb07b839cdf59a1acf
woosungking <abuga991008@gmail.com>
2024-11-04 21:31:13 +0900

    feat<OutputView> 결과값에 대한 레퍼 클래스

---
+++ src/main/java/com/java/repository/output/OutputValue.java
@@ -0,0 +1,13 @@
+package com.java.repository.output;
+
+public class OutputValue {
+    private final Integer outputValue;
+
+    public OutputValue(Integer outputValue) {
+        this.outputValue = outputValue;
+    }
+
+    public Integer getValue(){
+        return this.outputValue;
+    }
+}
