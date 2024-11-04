677756e8a85767462776bbbb93491684a76411a6
woosungking <abuga991008@gmail.com>
2024-11-04 23:01:48 +0900

    refactor<.generateDelimiterRegex> 정규표현식 생성 메서드
    기존 ValueExtractServiceImpl.extractValue 에서 담당하던 정규표현식 관련 로직을 DelimiterService로 분리

--- src/main/java/com/java/service/calculator/delimiter/DelimiterService.java
+++ src/main/java/com/java/service/calculator/delimiter/DelimiterService.java
@@ -2,7 +2,11 @@ package com.java.service.calculator.delimiter;
 
 import com.java.repository.delimiter.Delimiter;
 
+import java.util.List;
+
 public interface DelimiterService {
     public void extractCustomDelimiter(String inputValue);
     public void saveCustomDelimiter(Delimiter delimiter);
+    public String generateDelimiterRegex(List<Delimiter> delimiters);
+    public List<Delimiter> getDelimiterList();
 }
