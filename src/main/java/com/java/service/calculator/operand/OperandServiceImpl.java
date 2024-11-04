848912be87ffeef70c78ad217d00b9c25155d2f0
woosungking <abuga991008@gmail.com>
2024-11-04 23:19:27 +0900

    refactor<.convertStringListToOperand> 유틸

--- src/main/java/com/java/service/calculator/operand/OperandServiceImpl.java
+++ src/main/java/com/java/service/calculator/operand/OperandServiceImpl.java
@@ -4,6 +4,10 @@ import com.java.repository.delimiter.DelimiterRepository;
 import com.java.repository.operand.Operand;
 import com.java.repository.operand.OperandRepository;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class OperandServiceImpl implements OperandService {
     private final DelimiterRepository delimiterRepository;
     private final OperandRepository operandRepository;
@@ -14,10 +18,25 @@ public class OperandServiceImpl implements OperandService {
     }
 
     @Override
-    public void extractValue(String inputValue, String regex) {
+    public List<Operand> extractOperand(String inputValue, String regex) {
         String[] splitResult = inputValue.split(regex);
-        for(String operand : splitResult){
-            operandRepository.addOperand(new Operand(Integer.parseInt(operand)));
+        List<String> result = Arrays.asList(splitResult);
+        return convertStringListToOperand(result);
+    }
+
+    public void saveOperand(){}
+    public void saveOperandList(List<Operand> operandList){
+        for(Operand operand : operandList){
+            operandRepository.addOperand(operand);
         }
+    }
+
+
+    public List<Operand> convertStringListToOperand(List<String> strOperand){
+        List<Operand> operandList = new ArrayList<>();
+        for(String operand : strOperand){
+            operandList.add(new Operand(Integer.parseInt(operand)));
+        }
+        return operandList;
     }
 }
