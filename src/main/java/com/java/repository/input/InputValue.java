01b5f0132fbf9d738eca6a381051dfb48899ebeb
woosungking <abuga991008@gmail.com>
2024-11-04 21:30:04 +0900

    feat<InputValue> 초기 입력값에 대한 레퍼 클레스

---
+++ src/main/java/com/java/repository/input/InputValue.java
@@ -0,0 +1,13 @@
+package com.java.repository.input;
+
+public class InputValue {
+    private final String inputValue;
+
+    public InputValue(String inputValue) {
+        this.inputValue = inputValue;
+    }
+
+    public String getValue(){
+        return this.inputValue;
+    }
+}
