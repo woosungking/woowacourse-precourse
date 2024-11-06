a0deb61af012825c0d73f19c573f033e0f307c6e
woosungking <abuga991008@gmail.com>
2024-11-06 13:39:31 +0900

    feat<> INPUT_VALUE_ONLY_INTEGER 메시지 추가

--- src/main/java/com/java/exception/ErrorCode.java
+++ src/main/java/com/java/exception/ErrorCode.java
@@ -3,7 +3,8 @@ package com.java.exception;
 public enum ErrorCode {
     NO_ARG_INPUT("입력값이 아무것도 입력되지 않았습니다"),
     IS_ONLY_SPACE("스페이스만 입력이 되었습니다"),
-    CUSTOM_DELIMITER_IS_NOT_VALIDATED("커스텀 구분자는 문자여야 합니다.");
+    CUSTOM_DELIMITER_IS_NOT_VALIDATED("커스텀 구분자는 문자여야 합니다."),
+    INPUT_VALUE_ONLY_INTEGER("구분자를 제외한 모든 문자는 정수여야 합니다.");
 
     private final String message;
     ErrorCode(String message){
