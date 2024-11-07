012fcb5e3ec1a8b28e64d5c386bf9547ab88e664
woosungking <abuga991008@gmail.com>
2024-11-07 23:47:18 +0900

    test<.extractOperandTest> 정규식과 문자열로 정수만 뽑아내는 로직 테스트
    정규식을 입력을 할때 Pattern을 써서 하는게 안전함
    그냥 넣어서 했다가 고생을 많이 했음 이 부분은 주석처리했음

--- src/test/java/service/calculator/operand/OperandServiceImplTest.java
+++ src/test/java/service/calculator/operand/OperandServiceImplTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class OperandServiceImplTest {
     private  OperandRepository operandRepository;
@@ -32,6 +33,24 @@ public class OperandServiceImplTest {
         //then
         for(int i=0; i<actualValue.size(); i++){
             Assertions.assertEquals(actualValue.get(i).getValue(), expectedValue.get(i).getValue());
+        }
+    }
+
+    @Test
+    public void extractOperandTest(){
+        //given
+
+        //String regex = "\\Q,\\E|\\Q:\\E|\\Q//!\\n\\E|\\Q!\\E|\\Q//!\\n\\E";
+        String regex = Pattern.quote(",") + "|" +
+                Pattern.quote(":") + "|" +
+                Pattern.quote("//!\\n") + "|" +
+                Pattern.quote("!");
+
+        String inputValue = "1:,//!\\n";
+        //when
+        List<Operand> actualValue = operandService.extractOperand(inputValue, regex);
+        for (Operand value : actualValue){
+            System.out.println(value.getValue());
         }
     }
 
