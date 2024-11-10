d18fb5e7da2bb43648f7ed2628afa9ac1c48c763
woosungking <abuga991008@gmail.com>
2024-11-11 01:16:24 +0900

    refactor<<DelimiterList> Delimiter의 팩토리 매서드 사용으로 인한 코드 변경

--- src/main/java/com/java/repository/delimiter/DelimiterList.java
+++ src/main/java/com/java/repository/delimiter/DelimiterList.java
@@ -3,25 +3,26 @@ package com.java.repository.delimiter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DelimiterRepository {
+public class DelimiterList {
     private final List<Delimiter> delimiterRepo;
 
-    public DelimiterRepository() {
+    public DelimiterList() {
         delimiterRepo = new ArrayList<>();
-        delimiterRepo.add(new Delimiter(":"));
-        delimiterRepo.add(new Delimiter(","));
+        setDefaultDelimiters();
     }
-
     public void addDelimiter(Delimiter delimiter) {
         delimiterRepo.add(delimiter);
     }
     public void addAllDelimiter(List<Delimiter> delimiters){
-        for(Delimiter delimiter : delimiters){
-            addDelimiter(delimiter);
-        }
+        delimiterRepo.addAll(delimiters);
     }
 
     public List<Delimiter> getDelimiterRepo() {
         return delimiterRepo; // 수정 불가능한 리스트 반환
+    }
+
+    private void setDefaultDelimiters(){
+        delimiterRepo.add(Delimiter.of(","));
+        delimiterRepo.add(Delimiter.of(":"));
     }
 }
