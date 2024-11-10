6b4e7dc4c395126760b1307695a574a668793b6d
woosungking <abuga991008@gmail.com>
2024-11-10 20:51:34 +0900

    refactor<.convertOperendListToInteger> OperandList -> IntegerList로 변환하는 메서드 분리
    내부 클레스가 아닌 static 으로 선언.

--- src/main/java/com/java/service/calculator/calculate/CalculatorServiceImpl.java
+++ src/main/java/com/java/service/calculator/calculate/CalculatorServiceImpl.java
@@ -4,6 +4,7 @@ import com.java.repository.operand.Operand;
 import com.java.repository.operand.OperandRepository;
 import com.java.repository.output.OutputValue;
 import com.java.repository.output.OutputValueRepository;
+import com.java.service.calculator.calculate.util.CalculateUtil;
 import com.java.service.calculator.operand.OperandService;
 
 import java.util.List;
@@ -15,12 +16,8 @@ public class CalculatorServiceImpl implements CalculatorService{
     }
     @Override
     public Integer addOperandListValue(List<Operand> operand) { //여기는 딱 더하기만 해주자.
-        List<Integer> integerOperand =  convertOperendListToInteger(operand);
+        List<Integer> integerOperand = CalculateUtil.convertOperendListToInteger(operand);
         return integerOperand.stream().reduce(0, Integer::sum);
-    }
-
-    public List<Integer> convertOperendListToInteger(List<Operand> operandList){
-        return operandList.stream().map(Operand::getValue).toList();
     }
     
 }
