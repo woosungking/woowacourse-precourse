ce5019d80b5de9a97238832ac4a5f6d9de113d19
woosungking <abuga991008@gmail.com>
2024-11-11 00:03:39 +0900

    refactor<Delimiter> 객체 생성을 팩토리 메서드를 사용하게 변경

--- src/main/java/com/java/repository/delimiter/Delimiter.java
+++ src/main/java/com/java/repository/delimiter/Delimiter.java
@@ -1,9 +1,15 @@
 package com.java.repository.delimiter;
 
 public class Delimiter {
-    private final String value;
-    public Delimiter(String value){
+    private String value;
+    public Delimiter(){
+        //아무런 작업을 하지 않음.
+    }
+    private Delimiter(String value){
         this.value=value;
+    }
+    public static Delimiter of(String value){
+        return new Delimiter(value);
     }
     public String getValue(){
         return this.value;
