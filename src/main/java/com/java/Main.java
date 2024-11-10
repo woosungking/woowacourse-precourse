3080bbd30935d723457446cc80078a20a2f2a1b0
woosungking <abuga991008@gmail.com>
2024-11-11 01:15:43 +0900

    refactor<<ServiceFactory> 의존성 관리 클래스

--- src/main/java/com/java/Main.java
+++ src/main/java/com/java/Main.java
@@ -1,25 +1,9 @@
 package com.java;
 
 import com.java.controller.CalculatorController;
-import com.java.service.calculator.delimiter.DelimiterFacade;
-import com.java.service.calculator.delimiter.DelimiterFacadeImpl;
-import com.java.service.calculator.operand.OperandFacade;
-import com.java.service.calculator.operand.OperandFacadeImpl;
-import com.java.service.io.input.InputValueServiceImpl;
-import com.java.repository.delimiter.DelimiterRepository;
-import com.java.repository.input.InputValueRepository;
-import com.java.repository.operand.OperandRepository;
-import com.java.repository.output.OutputValueRepository;
+import com.java.config.ServiceFactory;
 import com.java.service.calculator.CalculatorFacade;
-import com.java.service.calculator.CalculatorFacadeImpl;
-import com.java.service.calculator.calculate.CalculatorService;
-import com.java.service.calculator.calculate.CalculatorServiceImpl;
-import com.java.service.calculator.delimiter.DelimiterService;
-import com.java.service.calculator.delimiter.DelimiterServiceImpl2;
-import com.java.service.calculator.operand.OperandService;
-import com.java.service.calculator.operand.OperandServiceImpl;
-import com.java.service.io.output.OutputValueService;
-import com.java.service.io.output.OutputValueServiceImpl;
+import com.java.service.io.input.InputValueServiceImpl;
 import com.java.view.InputView;
 import com.java.view.OutputView;
 
@@ -27,21 +11,18 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        InputValueRepository inputValueRepository = new InputValueRepository();
-        InputValueServiceImpl inputValueHandler = new InputValueServiceImpl(inputValueRepository);
-        DelimiterRepository delimiterRepository = new DelimiterRepository();
-        DelimiterService delimiterService = new DelimiterServiceImpl2(delimiterRepository);
-        OperandRepository operandRepository = new OperandRepository();
-        OutputValueRepository outputValueRepository = new OutputValueRepository();
-        OperandService operandService = new OperandServiceImpl(operandRepository);
-        OutputValueService outputValueService = new OutputValueServiceImpl(outputValueRepository);
-        CalculatorService calculatorService = new CalculatorServiceImpl();
-        OutputView outputView = new OutputView(outputValueService);
-        DelimiterFacade delimiterFacade = new DelimiterFacadeImpl(delimiterService);
-        OperandFacade operandFacade = new OperandFacadeImpl(operandService);
-        CalculatorFacade calculatorFacade = new CalculatorFacadeImpl(delimiterFacade, calculatorService, outputValueService, operandFacade);
-        InputView inputView = new InputView(inputValueHandler);
-        CalculatorController calculatorController = new CalculatorController(inputView,outputView,calculatorFacade);
+        // 팩토리 메서드를 사용하여 객체 생성
+        InputValueServiceImpl inputValueService = ServiceFactory.createInputValueService();
+        InputView inputView = ServiceFactory.createInputView(inputValueService);
+        OutputView outputView = ServiceFactory.createOutputView(ServiceFactory.createOutputValueService(ServiceFactory.createOutputValueRepository()));
+        CalculatorFacade calculatorFacade = ServiceFactory.createCalculatorFacade(
+                ServiceFactory.createDelimiterFacade(ServiceFactory.createDelimiterService(ServiceFactory.createDelimiterList())),
+                ServiceFactory.createCalculatorService(),
+                ServiceFactory.createOutputValueService(ServiceFactory.createOutputValueRepository()),
+                ServiceFactory.createOperandFacade(ServiceFactory.createOperandService(ServiceFactory.createOperandRepository()))
+        );
+
+        CalculatorController calculatorController = new CalculatorController(inputView, outputView, calculatorFacade);
         calculatorController.calculator();
     }
 }
---
+++ src/main/java/com/java/config/ServiceFactory.java
@@ -0,0 +1,78 @@
+package com.java.config;
+
+import com.java.repository.delimiter.DelimiterList;
+import com.java.repository.input.InputValueRepository;
+import com.java.repository.operand.OperandRepository;
+import com.java.repository.output.OutputValueRepository;
+import com.java.service.calculator.CalculatorFacade;
+import com.java.service.calculator.CalculatorFacadeImpl;
+import com.java.service.calculator.calculate.CalculatorService;
+import com.java.service.calculator.calculate.CalculatorServiceImpl;
+import com.java.service.calculator.delimiter.DelimiterFacade;
+import com.java.service.calculator.delimiter.DelimiterFacadeImpl;
+import com.java.service.calculator.delimiter.DelimiterService;
+import com.java.service.calculator.delimiter.DelimiterServiceImpl2;
+import com.java.service.calculator.operand.OperandFacade;
+import com.java.service.calculator.operand.OperandFacadeImpl;
+import com.java.service.calculator.operand.OperandService;
+import com.java.service.calculator.operand.OperandServiceImpl;
+import com.java.service.io.input.InputValueServiceImpl;
+import com.java.service.io.output.OutputValueService;
+import com.java.service.io.output.OutputValueServiceImpl;
+import com.java.view.InputView;
+import com.java.view.OutputView;
+
+public class ServiceFactory {
+
+    public static InputValueServiceImpl createInputValueService() {
+        return new InputValueServiceImpl(new InputValueRepository());
+    }
+
+    public static DelimiterList createDelimiterList() {
+        return new DelimiterList();
+    }
+
+    public static DelimiterService createDelimiterService(DelimiterList delimiterList) {
+        return new DelimiterServiceImpl2(delimiterList);
+    }
+
+    public static OperandRepository createOperandRepository() {
+        return new OperandRepository();
+    }
+
+    public static OutputValueRepository createOutputValueRepository() {
+        return new OutputValueRepository();
+    }
+
+    public static OperandService createOperandService(OperandRepository operandRepository) {
+        return new OperandServiceImpl(operandRepository);
+    }
+
+    public static OutputValueService createOutputValueService(OutputValueRepository outputValueRepository) {
+        return new OutputValueServiceImpl(outputValueRepository);
+    }
+
+    public static CalculatorService createCalculatorService() {
+        return new CalculatorServiceImpl();
+    }
+
+    public static DelimiterFacade createDelimiterFacade(DelimiterService delimiterService) {
+        return new DelimiterFacadeImpl(delimiterService);
+    }
+
+    public static OperandFacade createOperandFacade(OperandService operandService) {
+        return new OperandFacadeImpl(operandService);
+    }
+
+    public static CalculatorFacade createCalculatorFacade(DelimiterFacade delimiterFacade, CalculatorService calculatorService, OutputValueService outputValueService, OperandFacade operandFacade) {
+        return new CalculatorFacadeImpl(delimiterFacade, calculatorService, outputValueService, operandFacade);
+    }
+
+    public static InputView createInputView(InputValueServiceImpl inputValueHandler) {
+        return new InputView(inputValueHandler);
+    }
+
+    public static OutputView createOutputView(OutputValueService outputValueService) {
+        return new OutputView(outputValueService);
+    }
+}
